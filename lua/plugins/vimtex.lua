cba51e6d701755b4cfcdf73c33071e87005f29b4
go-bayes <joseph.bulbulia@gmail.com>
2026-02-02 18:14:52 +1300

    add vimtex compiler options for bibtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,4 +1,9 @@
-  return {
-    "lervag/vimtex",
-    ft = "tex",
-  }
+return {
+  "lervag/vimtex",
+  ft = "tex",
+  config = function()
+    vim.g.vimtex_compiler_latexmk = {
+      options = { "-pdf", "-bibtex", "-interaction=nonstopmode", "-synctex=1" },
+    }
+  end,
+}
