10e3fe67e747b1f36da6027fc971da9ebb2f6dff
go-bayes <joseph.bulbulia@gmail.com>
2026-02-03 22:21:04 +1300

    add soft line breaks for tex files

---
+++ after/ftplugin/tex.lua
@@ -0,0 +1,4 @@
+-- soft line breaks for latex documents
+vim.opt_local.wrap = true -- enable visual line wrapping
+vim.opt_local.linebreak = true -- break at word boundaries
+vim.opt_local.breakindent = true -- indent wrapped lines to match
